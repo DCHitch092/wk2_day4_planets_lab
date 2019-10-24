c52cfc1e2a3141cf18f80569c985399acad0d2d1
Richard <fahitch+github@gmail.com>
2019-10-24 13:55:36 +0100

    we did it

--- star_system.rb
+++ star_system.rb
@@ -28,9 +28,8 @@ class StarSystem
 
  def get_planets_with_more_moons(num)
    result = @planets.find_all { |planet| planet.number_of_moons > num }
-   p result.map { |result| p result.name }
+   result.map { |result| p result.name }
 
-    # p result.planet_names
  end
 
  def get_number_of_planets_closer_than(num)
@@ -40,8 +39,23 @@ class StarSystem
 
  def get_total_number_of_moons
    @planets.reduce(0) { |list, planet| list + planet.number_of_moons}
+ end
+
+ def get_planet_names_sorted_by_increasing_distance_from_sun
+  result = @planets.sort_by { |planet| planet.distance_from_sun}
+  result.map { |result| result.name }
 
  end
+
+ def get_planet_names_sorted_by_size_decreasing
+   result = @plants.sort_by { |planet| planet.diameter}
+   result.reverse.map { |result| result.name}
+
+   # result = @planets.sort_by { |planet| planet.diameter}.reverse!
+   # result.map { |result| result.name}
+
+ end
+
  #   @planets.find_all do | planet|
  #     planet.number_of_moons >= 4
  #   end
